78c92ee9eba13c30c165c61f777d00a20ebb4906
Barnaby <accounts@southcla.ws>
2022-06-30 13:57:25 +0100

    make optional types nullish

--- custom/optional/optional.go
+++ custom/optional/optional.go
@@ -10,6 +10,6 @@ import (
 var (
 	OptionalType = "4d63.com/optional.Optional"
 	OptionalFunc = func(c *supervillain.Converter, t reflect.Type, s string, g string, i int) string {
-		return fmt.Sprintf("%s.optional()", c.ConvertType(t.Elem(), s, i))
+		return fmt.Sprintf("%s.optional().nullish()", c.ConvertType(t.Elem(), s, i))
 	}
 )
